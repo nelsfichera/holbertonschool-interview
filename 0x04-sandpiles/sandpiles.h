17208acf79a54defa9113b60e491b0d8025db55c
Nels <nelsfichera@gmail.com>
2022-01-13 22:27:38 -0500

    something to grade

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,13 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+int stable(int grid[3][3]);
+
+void topple(int grid1[3][3], int grid2[3][3]);
+
+#endif /* SANDPILES_H */
